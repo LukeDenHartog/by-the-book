a951fd8367908f1ccb744daa051073842683dd57
Luke <121516961+Afrozez@users.noreply.github.com>
2024-06-24 10:40:15 -0700

    Updating the className to better reflect its intended purpose.

--- src/InteractiveInterface.js
+++ src/InteractiveInterface.js
@@ -22,7 +22,7 @@ function UncontrolledExample() {
             
             <p className="carouselParagraph">The main objective is to have a hand value closer to 21 than the dealer's hand without exceeding 21.</p>
           </div>
-          <Button className="interfaceButton"variant="primary">Learn More</Button>{' '}
+          <Button className="interfaceLearnButton"variant="primary">Learn More</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -36,7 +36,7 @@ function UncontrolledExample() {
             <h3>Basic Strategy</h3>
             <p className="carouselParagraph">A widely-used approach that determines the optimal move for every hand—whether to hit, stand, double, or split.</p>
           </div>
-          <Button className="interfaceButton" variant="primary">Learn More</Button>{' '}
+          <Button className="interfaceLearnButton" variant="primary">Learn More</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -50,7 +50,7 @@ function UncontrolledExample() {
           <h3>Card Counting</h3>
           <p className="carouselParagraph">Monitoring the distribution of high and low cards to tilt the odds in favor of the player.</p>
           </div>
-          <Button className="interfaceButton" variant="primary">Learn More</Button>{' '}
+          <Button className="interfaceLearnButton" variant="primary">Learn More</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -64,7 +64,7 @@ function UncontrolledExample() {
           <h3>Get help with your addiction</h3>
           <p className="carouselParagraph">Gambling addiction is a compulsive and uncontrollable urge to gamble despite severe consequences.</p>
           </div>
-          <Button className="interfaceButton" variant="primary">Learn More</Button>{' '}
+          <Button className="interfaceLearnButton" variant="primary">Learn More</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -78,7 +78,7 @@ function UncontrolledExample() {
           <h3>Sports Betting Advice</h3>
           <p className="carouselParagraph">Some helpful advice on navigating the sportsbooks.</p>
           </div>
-          <Button className="interfaceButton" variant="primary">Learn More</Button>{' '}
+          <Button className="interfaceLearnButton" variant="primary">Learn More</Button>{' '}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
