94370cff891ca9b299a9643bbef6e4856605c034
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-08 14:22:14 -0700

    adding icons and hover pseudo-class

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Footer.css';
 import FooterCardImage from './Footer-Img.js';
+import linkedinIcon from '../../assets/images/footerImages/icons8-linkedin-50.png';
+import githubIcon from '../../assets/images/footerImages/icons8-github-64.png';
+
 
 export default function Footer() {
     return  (
@@ -12,6 +15,8 @@ export default function Footer() {
                         <a href="#Map" className="nav-footer-link"><p>Site Map</p></a>
                 </section>
                 <FooterCardImage/>
+                <img src={linkedinIcon} alt="LinkedIn Icon" className="footer-icon" />
+                <img src={githubIcon} alt="GitHub Icon" className="footer-icon" />
                 <p> © 2023 Luke A DenHartog. All Rights Reserved.</p>
                 <p>Have a gambling addiction? <a href="#Home">Get help here!</a></p>
                 <p>All trademarks referenced herein are the properties of their respective owners.</p>
