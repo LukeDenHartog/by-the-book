4308fa48f82cc46767b97863efe8d8735dabb91e
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-07 20:15:24 -0700

    adding links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,11 @@ export default function Footer() {
     return  (
         <div className="footer-container">
             <footer className="footer">
+            <section>
+                    <a href="#About" className="nav-footer-link"><p>About</p></a>
+                    <a href="#Contact" className="nav-footer-link"><p>Contact Me</p></a>
+                    <a href="#Map" className="nav-footer-link"><p>Site Map</p></a>
+                </section>
                 <p> © 2023 Luke A DenHartog. All Rights Reserved.</p>
                 <p>Have a gambling addiction? <a href="#Home">Get help here!</a></p>
                 <p>All trademarks referenced herein are the properties of their respective owners.</p>
