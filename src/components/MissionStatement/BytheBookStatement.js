c29525a5ab4e801c06a6e6d2c0e2772616dfbf53
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-07 21:58:33 -0700

    adding content/styling to the quick statement

--- src/components/MissionStatement/BytheBookStatement.js
+++ src/components/MissionStatement/BytheBookStatement.js
@@ -5,8 +5,8 @@ function QuickMissionStatement() {
   return (
     <div className="quick-mission-statement-wrapper">
       <section className="quick-mission-statement-text">
-        <p>We believe that informed gambling is the best approach. While we advocate for avoiding casinos altogether and instead pursuing a stable career and investing in safer markets that generate compound interest, our mission is to educate new players on the nuances of blackjack through effective strategies. By learning the basics, we aim to create more educated and confident players at the casino.</p>
-      </section>
+        <p>We believe that informed gambling is the best approach. While <span id="avoid-casinos-text">we advocate for avoiding casinos altogether</span> and <span id="compound-interest-text">we encourage pursuing a stable job and investing in safer markets that generate compound interest</span>, our mission is to educate new players on the nuances of blackjack through effective strategies. By learning the basics, we aim to create more educated and confident players at the casino.</p>
+      </section> 
     </div>
   );
 }
