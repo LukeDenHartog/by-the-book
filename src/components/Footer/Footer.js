878d94d0e3645b9e8be3f218073fea8b4bb4ed4d
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-07 20:13:43 -0700

    adding footer img

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Footer.css'
 
+
 export default function Footer() {
     return  (
-        <footer className="footer">
-            <p> © 2023 Luke A DenHartog. All Rights Reserved.</p>
-            <p>Have a gambling addiction? <a href="#Home">Get help here!</a></p>
-      </footer>
+        <div className="footer-container">
+            <footer className="footer">
+                <p> © 2023 Luke A DenHartog. All Rights Reserved.</p>
+                <p>Have a gambling addiction? <a href="#Home">Get help here!</a></p>
+                <p>All trademarks referenced herein are the properties of their respective owners.</p>
+            </footer>
+        </div>
     )
 }
