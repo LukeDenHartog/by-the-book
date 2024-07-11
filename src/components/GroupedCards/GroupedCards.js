b70c7feb17c3898e1277f3d85b2adb2d7c604360
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-11 05:40:57 -0700

    adding links to cards

--- src/components/GroupedCards/GroupedCards.js
+++ src/components/GroupedCards/GroupedCards.js
@@ -15,51 +15,67 @@ function ImageAndText() {
   return (
     <div className="card-container">
       <Card className="custom-cards">
-        <Card.Img src={holdingCardsImg} alt="Card image" />
-        <Card.ImgOverlay>
-          <button className="button">Blackjack History</button>
-        </Card.ImgOverlay>
+        <a href="https://www.stationcasinosblog.com/2015/06/32-vs-65-blackjack-a-devil-in-the-details/">
+          <Card.Img src={holdingCardsImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">Blackjack History</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>
       <Card className="custom-cards">
-        <Card.Img src={jackAceThreeChipsImg} alt="Card image" />
-        <Card.ImgOverlay>
-        <button className="button">Learn from Legends</button>
-        </Card.ImgOverlay>
+        <a href="https://www.nytimes.com/2012/03/10/sports/a-card-counting-mix-of-bibles-blackjack-and-cash.html">
+          <Card.Img src={jackAceThreeChipsImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">Learn from Legends</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>
       <Card className="custom-cards">
-        <Card.Img src={vegasBirdsEyeImg} alt="Card image" />
-        <Card.ImgOverlay>
-          <button className="button">1st Vegas experiences</button>
-        </Card.ImgOverlay>
+        <a href="https://www.canyontours.com/guides/15-tips-for-your-first-trip-to-las-vegas/">
+          <Card.Img src={vegasBirdsEyeImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">1st Vegas experiences</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>
       <Card className="custom-cards">
-        <Card.Img src={ultimateBjTable} alt="Card image" />
-        <Card.ImgOverlay>
-          <button className="button">Good Vs Bad Tables</button>
-        </Card.ImgOverlay>
+        <a href="https://www.stationcasinosblog.com/2015/06/32-vs-65-blackjack-a-devil-in-the-details/">
+          <Card.Img src={ultimateBjTable} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">Good Vs Bad Tables</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>      <Card className="custom-cards">
-        <Card.Img src={ParisHotelImg} alt="Card image" />
-        <Card.ImgOverlay>
-          <button className="button">Vegas Content Creators</button> 
-        </Card.ImgOverlay>
+        <a href="https://www.youtube.com/@NormaGeli/">
+          <Card.Img src={ParisHotelImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">Vegas Content Creators</button> 
+          </Card.ImgOverlay>
+        </a>
       </Card>
       <Card className="custom-cards">
-        <Card.Img src={pokerChipsImg} alt="Card image" />
-        <Card.ImgOverlay>
-          <button className="button">Buy a Blackjack Table</button>
-        </Card.ImgOverlay>
+        <a href="https://www.casinosupply.com/">
+          <Card.Img src={pokerChipsImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">Buy a Blackjack Table</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>
       <Card className="custom-cards">
-        <Card.Img src={franziskaBuffetImg} alt="Card image" />
-        <Card.ImgOverlay>
-        <button className="button">What to eat in Vegas</button>
-        </Card.ImgOverlay>
+        <a href="https://www.caesars.com/caesars-palace/restaurants/bacchanal-buffet">
+          <Card.Img src={franziskaBuffetImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">What to eat in Vegas</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>
-      <Card className="custom-cards">
-        <Card.Img src={vegasSignImg} alt="Card image" />
-        <Card.ImgOverlay>
-          <button className="button">Vegas Weather Forecast</button>
-        </Card.ImgOverlay>
+        <Card className="custom-cards">
+        <a href="https://www.wunderground.com/weather/us/nv/las-vegas">
+          <Card.Img src={vegasSignImg} alt="Card image" />
+          <Card.ImgOverlay>
+            <button className="button">Vegas Weather Forecast</button>
+          </Card.ImgOverlay>
+        </a>
       </Card>
     </div>
   );
