fc14f5f7534527c6d64f4ce01c2082c413595c8c
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-12 10:52:16 -0700

    adjusting module format on lunch break

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Custom Pages/Components
-import Menubar from "./components/Navbar/Navbar.js";
-import Footer from "./components/Footer/Footer.js";
-import Carousel from "./pages/Home/Carousel.js";
-import ContentGroup from "./components/GroupedCards/GroupedCards.js";
-import ByTheBookStatement from "./components/MissionStatement/BytheBookStatement.js";
+import LandingPage from "../src/pages/Home/LandingPage"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Menubar />
-    <Carousel />
-    <ContentGroup />
-    <ByTheBookStatement />
-    <Footer />
+    <LandingPage/>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/pages/Home/LandingPage.js
@@ -0,0 +1,19 @@
+import Menubar from "../../components/Navbar/Navbar.js";
+import Footer from "../../components/Footer/Footer.js";
+import Carousel from "../../pages/Home/Carousel.js";
+import ContentGroup from "../../components/GroupedCards/GroupedCards.js";
+import ByTheBookStatement from "../../components/MissionStatement/BytheBookStatement.js";
+
+function LandingPageFunction() {
+    return (
+        <>
+        <Menubar />
+        <Carousel />
+        <ContentGroup />
+        <ByTheBookStatement />
+        <Footer />
+        </>
+    )
+}
+
+export default LandingPageFunction;
