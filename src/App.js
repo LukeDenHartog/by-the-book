c0636634b4fee20262afe1c71a973a1909271f68
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-14 22:21:38 -0700

    adding App.js file

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// Custom Pages/Components
+import LandingPage from "../src/pages/Home/LandingPage"
+
+function App() {
+    return (
+        <LandingPage />
+    );
+  }
+  
+  export default App;
