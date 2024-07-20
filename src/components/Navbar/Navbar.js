3a64c12e5a2755d08a9809e72a43bf51ebfe67e5
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-20 14:48:54 -0700

    hover effect & sizing added to offcanvas text

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,14 +17,16 @@ function NavbarModule() {
             <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg"><h2 className="brand-name-color">By the Book</h2></Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="me-auto">
-              <NavDropdown title="Blackjack (21)" id="collapsible-nav-dropdown">
+            <Nav>
+              <NavDropdown title={<h3>Black Jack (21)</h3>} id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Basic Rules</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Basic Strategy</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Counting Cards</NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Gambling Advice" id="collapsible-nav-dropdown">
+              </Nav>
+              <Nav>
+              <NavDropdown title={<h3>Gambling Advice</h3>} id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Betting</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Sports</NavDropdown.Item>
                 <NavDropdown.Divider />
@@ -32,17 +34,19 @@ function NavbarModule() {
               </NavDropdown>
             </Nav>
             <Nav>
-              <NavDropdown title="About" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">News</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">About By the Book</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">My GitHub Profile</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Account" id="collapsible-nav-dropdown">
+            <NavDropdown title={<h3>Account</h3>} id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Sign out</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <NavDropdown title={<h3>About</h3>} id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">News</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">About By the Book</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">My GitHub Profile</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Offcanvas.Body>
