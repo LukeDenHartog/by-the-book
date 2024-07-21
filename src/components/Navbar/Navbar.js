9ebc27bf08d5dfaca4c4b61d3fd9ac1e09679d65
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-21 14:12:11 -0700

    Created toggle functions for OffCanvas feature and it's now visible based on screen width

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,81 @@
 import "./Navbar.css";
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Navbar, Container, Offcanvas, Nav, NavDropdown } from 'react-bootstrap';
 
 function NavbarModule() {
+  const [showOffCanvas, setShowOffCanvas] = useState(false);
+
+  // If showOffCanvas changes, the handleResize function is updated.
+  const handleResize = useCallback(() => {
+    if (window.innerWidth > 993 && showOffCanvas) {
+      setShowOffCanvas(false);
+    }
+  }, [showOffCanvas]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+      // The cleanup function is for preventing memory leaks and avoiding multiple event listeners being attached if the effect re-runs.
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  const handleShowOffCanvas = () => setShowOffCanvas(true);
+  const handleCloseOffCanvas = () => setShowOffCanvas(false);
+
   return (
     <Navbar expand="lg" className="mb-3 nav-background">
       <Container fluid>
-        <Navbar.Brand href="#" className="brand-name-color"><h1>By the Book</h1></Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+        <Navbar.Brand href="#" className="brand-name-color">
+          <h2>By the Book</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" onClick={handleShowOffCanvas} />
         <Navbar.Offcanvas
           id="offcanvasNavbar-expand-lg"
           aria-labelledby="offcanvasNavbarLabel-expand-lg"
           placement="end"
+          show={showOffCanvas}
+          onHide={handleCloseOffCanvas}
         >
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg"><h2 className="brand-name-color">By the Book</h2></Offcanvas.Title>
+            <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+              <h2 className="brand-name-color">By the Book</h2>
+            </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav>
-              <NavDropdown title={<h3>Black Jack (21)</h3>} id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Basic Rules</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Basic Strategy</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Counting Cards</NavDropdown.Item>
-              </NavDropdown>
+            <Nav className="me-auto">
+              <Nav className="off-canvas-hover-feature">
+                <NavDropdown title={<h4>Black Jack (21)</h4>} id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Basic Rules</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Basic Strategy</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Counting Cards</NavDropdown.Item>
+                </NavDropdown>
               </Nav>
-              <Nav>
-              <NavDropdown title={<h3>Gambling Advice</h3>} id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Betting</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Sports</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.3">Support for Gambling Addiction</NavDropdown.Item>
-              </NavDropdown>
+              <Nav className="off-canvas-hover-feature">
+                <NavDropdown title={<h4>Gambling Advice</h4>} id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Betting</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Sports</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">Support for Gambling Addiction</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
             </Nav>
-            <Nav>
-            <NavDropdown title={<h3>Account</h3>} id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Sign out</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <NavDropdown title={<h3>About</h3>} id="collapsible-nav-dropdown">
+            <Nav className="off-canvas-hover-feature">
+              <NavDropdown title={<h4>About us</h4>} id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">News</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">About By the Book</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">My GitHub Profile</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="off-canvas-hover-feature">
+            <NavDropdown title={<h4>Account</h4>} id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Sign out</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Offcanvas.Body>
@@ -54,6 +83,6 @@ function NavbarModule() {
       </Container>
     </Navbar>
   );
-};
+}
 
 export default NavbarModule;
