5c93fac067cebe9ef6dc3b702123985d32885966
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-22 12:54:31 -0700

    adding styling to dropdown items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import "./Navbar.css";
 import React, { useState, useEffect, useCallback } from 'react';
 import { Navbar, Container, Offcanvas, Nav, NavDropdown } from 'react-bootstrap';
-import downArrowImg from './icons8-chevron-24.png';
+import downArrowImg from './down-arrow-image.png';
 
 function NavbarModule() {
   const [showOffCanvas, setShowOffCanvas] = useState(false);
   // If showOffCanvas changes, the handleResize function is updated.
   const handleResize = useCallback(() => {
-    if (window.innerWidth > 993 && showOffCanvas) {
+    if (window.innerWidth > 992 && showOffCanvas) {
       setShowOffCanvas(false);
     }
   }, [showOffCanvas]);
@@ -45,34 +45,38 @@ function NavbarModule() {
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="me-auto">
-              <Nav className="off-canvas-hover-feature">
+              <Nav className="off-canvas-styling">
                 <NavDropdown title={<><h5>Black Jack (21)</h5><img src={downArrowImg} alt="Down Arrow" className="down-arrow" /></>} id="collapsible-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Basic Rules</NavDropdown.Item>
+                  <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.2">Basic Strategy</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">Counting Cards</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
-              <Nav className="off-canvas-hover-feature">
+              <Nav className="off-canvas-styling">
                 <NavDropdown title={<><h5>Gambling Advice</h5><img src={downArrowImg} alt="Down Arrow" className="down-arrow" /></>} id="collapsible-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Betting</NavDropdown.Item>
+                  <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.2">Sports</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.3">Support for Gambling Addiction</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
             </Nav>
-            <Nav className="off-canvas-hover-feature">
+            <Nav className="off-canvas-styling">
               <NavDropdown title={<><h5>About us</h5><img src={downArrowImg} alt="Down Arrow" className="down-arrow" /></>} id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">News</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">News</NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.2">About By the Book</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">My GitHub Profile</NavDropdown.Item>
               </NavDropdown>
             </Nav>
-            <Nav className="off-canvas-hover-feature" id="last-nav-element">
+            <Nav className="off-canvas-styling" id="last-nav-element">
               <NavDropdown title={<><h5>Account</h5><img src={downArrowImg} alt="Down Arrow" className="down-arrow" /></>} id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.2">Sign out</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
