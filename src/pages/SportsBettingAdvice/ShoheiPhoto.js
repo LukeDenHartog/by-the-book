ab6d7227b9154b0d1f3616b78190c3f6d201039c
Luke <121516961+Afrozez@users.noreply.github.com>
2024-07-29 14:57:15 -0700

    adding fluid img of ohtani/adjusted box-shadow colors

---
+++ src/pages/SportsBettingAdvice/ShoheiPhoto.js
@@ -0,0 +1,10 @@
+import Image from 'react-bootstrap/Image';
+import OhtaniImg from "../../assets/images/AdvicePagePhotos/ShoheiOhtani-ErikDrost.jpeg"
+
+function FluidExample() {
+
+  return <Image id="ohtani-image"src={OhtaniImg} fluid />;
+
+}
+
+export default FluidExample;
